2cb2dae644acdb5ddd01e5d58de308cbb1012d58
storm-beyndtech <khameleonstormstorm@gmail.com>
2025-07-01 18:37:47 +0100

    fly

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.use("/api/utils", utilsRoutes);
 app.use("/api/kycs", kycsRoutes);
 
 // Listening to port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 app.get("/", (req, res) => {
